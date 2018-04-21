73963629f24a4ac8f506ddf5a2d27ae0ab622de0
qiuqingwen <qqw@uplink.net.cn>
2018-04-21 13:27:37 +0800

    .

---
+++ Lumos.BLL/Service/ShippingAddressService.cs
@@ -0,0 +1,28 @@
+﻿using Lumos.Entity;
+using Lumos.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Lumos.BLL.Service
+{
+    public class ShippingAddressService : BaseProvider
+    {
+        public CustomJsonResult Edit(int operater, ShippingAddress shippingAddress)
+        {
+            CustomJsonResult result = new CustomJsonResult();
+
+            using (TransactionScope ts = new TransactionScope())
+            {
+
+
+            }
+
+
+            return result;
+        }
+    }
+}

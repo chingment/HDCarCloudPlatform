eec998c6f73650d41b9523f00e25255972225fcd
qiuqingwen <qqw@uplink.net.cn>
2018-05-24 12:03:30 +0800

    .

--- WebAppApi/Controllers/PayController.cs
+++ WebAppApi/Controllers/PayController.cs
@@ -43,22 +43,6 @@ namespace WebAppApi.Controllers
             stream.Seek(0, SeekOrigin.Begin);
             string postData = new StreamReader(stream).ReadToEnd();
 
-
-
-            string key = "test";
-            string secret = "6ZB97cdVz211O08EKZ6yriAYrHXFBowC";
-            long timespan = (long)(DateTime.Now - TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1))).TotalSeconds;
-            string signStr = Signature.Compute(key, secret, timespan, postData);
-
-
-            Dictionary<string, string> headers = new Dictionary<string, string>();
-            headers.Add("key", key);
-            headers.Add("timestamp", timespan.ToString());
-            headers.Add("sign", signStr);
-            HttpUtil http = new HttpUtil();
-            string result = http.HttpPostJson("http://120.79.233.231/api/Pay/ReceiveNotifyByStar", postData, headers);
-
-
             Log.Info("ReceiveNotify：" + postData);
 
             if (string.IsNullOrEmpty(postData))

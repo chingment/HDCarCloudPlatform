ba07129c09fa90417b753f407e23199c797574e6
qiuqingwen <qqw@uplink.net.cn>
2018-05-27 12:10:52 +0800

    .

--- WebAppApi/Controllers/ResourceController.cs
+++ WebAppApi/Controllers/ResourceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppApi.Models;
 using WebAppApi.Models.Resource;
 
 namespace WebAppApi.Controllers
@@ -33,7 +34,27 @@ namespace WebAppApi.Controllers
             return View(model);
         }
 
+        public ViewResult IdRecogInfo(string idnum)
+        {
+            //DetailsViewModelByBanner model = new DetailsViewModelByBanner(id);
 
+            List<IdRecogModel> models = new List<IdRecogModel>();
+
+
+            models.Add(new IdRecogModel { IdNum = "448837222031", Company = "广州永发皮具有限公司", ImgUrl = "~/Images/baobao2.jpg", Producer = "昌名代理生产", PuctionDate = "2018-05-27" });
+            models.Add(new IdRecogModel { IdNum = "548837554026", Company = "广州永发皮具有限公司", ImgUrl = "~/Images/baobao2.jpg", Producer = "龙胜代理生产", PuctionDate = "2018-05-14" });
+            models.Add(new IdRecogModel { IdNum = "844673554063", Company = "广州永发皮具有限公司", ImgUrl = "~/Images/baobao2.jpg", Producer = "明仔代理生产", PuctionDate = "2018-05-05" });
+
+
+
+            IdRecogModel model = models.Where(m => m.IdNum == idnum).FirstOrDefault();
+            if (model == null)
+            {
+                model = models[0];
+            }
+
+            return View(model);
+        }
 
     }
 }

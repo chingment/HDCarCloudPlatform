b9e102715b16665f5a29a406bf2d0e4de875c4d4
qiuqingwen <qqw@uplink.net.cn>
2018-07-17 10:52:24 +0800

    .

--- YdtSdk/YdtApi.cs
+++ YdtSdk/YdtApi.cs
@@ -40,7 +40,7 @@ namespace YdtSdk
     public class YdtApi : IYdtApi
     {
 
-        public string serverUrl = "http://119.23.141.14";
+        public string serverUrl = "http://119.23.141.14:6100";
 
 
         public YdtApi()
